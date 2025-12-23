23182cf9e88eb045764726ff9b1ee62fe03322fd
Darshan Karagir <karagirdarshan1@gmail.com>
2025-12-23 11:33:12 +0530

    Redis basics

---
+++ src/main/java/com/example/journalApp/confug/RedisConfug.java
@@ -0,0 +1,22 @@
+package com.example.journalApp.confug;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfug {
+
+    @Bean
+    public RedisTemplate RedisTamplate(RedisConnectionFactory factory){
+        RedisTemplate redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+}
